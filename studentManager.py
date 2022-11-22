da5d69d3e7abafa3cacb70617ba4eac0840ebd41
SamuelBasseyJohn <88030818+SamuelBasseyJohn@users.noreply.github.com>
2022-11-22 09:04:51 +0100

    Cleaned up UI

--- studentManager.py
+++ studentManager.py
@@ -2,49 +2,74 @@ from tkinter import *
 
 studentList = []
 
-count = 5
+count = 6
 root = Tk()
 
 
-def onClicked(row=count):
+def nameField(student, row):
+    nameText = Label(
+        root, text=f"Name: {student['name']} {student['surname']}")
+    nameText.grid(column=0, row=row)
+
+
+def matricNoField(student, row):
+    matricNoText = Label(
+        root, text=f"MatricNo: {student['matricNo']}")
+    matricNoText.grid(column=1, row=row)
+
+
+def scoreField(student, row):
+    scoreText = Label(
+        root, text=f"Score: {student['score']}")
+    scoreText.grid(column=2, row=row)
+
+
+def onClicked():
+    global count
     student = {
         'name': name.get(),
         'surname': surname.get(),
         'matricNo': matricNo.get(),
         'score': score.get(),
     }
+
     studentList.append(student)
+
     name.delete(0, END)
     surname.delete(0, END)
     matricNo.delete(0, END)
     score.delete(0, END)
 
-    studentText = Label(
-        root, text=f"Name: {student['name']} , Surname: {student['surname']} , MatricNo: {student['matricNo']} , Score: {student['score']}")
-    studentText.grid(column=1, row=row)
-    row += 1
+    nameField(student=student, row=count)
+    matricNoField(student=student, row=count)
+    scoreField(student=student, row=count)
+
+    count += 1
 
 
 nameText = Label(root, text="Name: ")
 nameText.grid(column=0, row=0)
-name = Entry(root)
+name = Entry(root, width=30)
 name.grid(column=1, row=0)
 surnameText = Label(root, text="Surname: ")
 surnameText.grid(column=0, row=1)
-surname = Entry(root)
+surname = Entry(root, width=30)
 surname.grid(column=1, row=1)
 matricNoText = Label(root, text="MatricNo: ")
 matricNoText.grid(column=0, row=2)
-matricNo = Entry(root)
+matricNo = Entry(root, width=30)
 matricNo.grid(column=1, row=2)
 scoreText = Label(root, text="Score: ")
 scoreText.grid(column=0, row=3)
-score = Entry(root)
+score = Entry(root, width=30)
 score.grid(column=1, row=3)
+whiteSpace1 = Label(root, text=" ")
+whiteSpace1.grid(column=0, row=4)
+
 
 button = Button(text="Compute", command=onClicked)
-button.grid(column=1, row=count + 1)
+button.grid(column=0, row=5)
 button = Button(text="Done", command=onClicked)
-button.grid(column=1, row=4)
+button.grid(column=1, row=5)
 
 root.mainloop()
